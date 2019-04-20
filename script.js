baf5e00da3825508ee38d05e2f5a9e1413559e49
mengru <4momolmr@gmail.com>
2019-04-20 10:39:07 +0800

    part1_finish

--- script.js
+++ script.js
@@ -5,18 +5,26 @@
 const image = document.getElementsByClassName("choice-grid")[0].children;
 const image_1 = document.getElementsByClassName("choice-grid")[1].children;
 const image_2 = document.getElementsByClassName("choice-grid")[2].children;
-const result = document.getElementsByClassName("result_button")[0];
+const quiz = document.getElementsByClassName("quiz")[0];
+const quiz_title = document.getElementsByClassName("quiz_title")[0];
+const quiz_desc = document.getElementsByClassName("quiz_desc")[0];
+const restart = document.getElementsByClassName("quiz_button")[0];
 var flag = Boolean(false);
 var flag_1 = Boolean(false);
 var flag_2 = Boolean(false);
+const map = {
+  one: "",
+  two: "",
+  three: ""
+}
 
+console.log(result);
 for(let val of image)
 {
   //console.log(val);
 
   val.addEventListener("click", function clickDiv(){
 
-    //console.log("Hello World!");
     val.getElementsByClassName("checkbox")[0].src="./images/checked.png";
     for(let val of image){
       val.classList.toggle("opacity", true);
@@ -26,8 +34,12 @@ for(let val of image)
     val.getElementsByClassName("checkbox")[0].src="./images/checked.png";
     val.classList.toggle("opacity", false);
     val.classList.toggle("click_effect", true);
+    map.one=val.getAttribute("data-choice-id");
+    console.log(map.one);
     flag=true;
     console.log(flag);
+    if(flag==true&&flag_1==true&&flag_2==true)
+      result();
   });
 }
 
@@ -44,15 +56,17 @@ for(let val of image_1)
     val.getElementsByClassName("checkbox")[0].src="./images/checked.png";
     val.classList.toggle("opacity", false);
     val.classList.toggle("click_effect", true);
+    map.two=val.getAttribute("data-choice-id");
+    console.log(map.two);
     flag_1=true;
-    console.log(flag_1);
+    if(flag==true&&flag_1==true&&flag_2==true)
+      result();
   });
 }
 
 for(let val of image_2)
 {
 
-  val.getAttribute("data-choice-id");
   val.addEventListener("click", function clickDiv(){
 
     for(let val of image_2){
@@ -63,7 +77,44 @@ for(let val of image_2)
     val.getElementsByClassName("checkbox")[0].src="./images/checked.png";
     val.classList.toggle("opacity", false);
     val.classList.toggle("click_effect", true);
+    map.three=val.getAttribute("data-choice-id");
+    console.log(map.three);
     flag_2=true;
-    console.log(flag_2);
+    if(flag==true&&flag_1==true&&flag_2==true)
+      result();
   });
+}
+
+restart.addEventListener("click", function clickDiv(){
+
+  for(let val of image){
+    val.classList.toggle("opacity", false);
+    val.classList.toggle("click_effect", false);
+    val.getElementsByClassName("checkbox")[0].src="./images/unchecked.png";
+  }
+  for(let val of image_1){
+    val.classList.toggle("opacity", false);
+    val.classList.toggle("click_effect", false);
+    val.getElementsByClassName("checkbox")[0].src="./images/unchecked.png";
+  }
+  for(let val of image_2){
+    val.classList.toggle("opacity", false);
+    val.classList.toggle("click_effect", false);
+    val.getElementsByClassName("checkbox")[0].src="./images/unchecked.png";
+  }
+
+  flag=flag_1=flag_2=false;
+  map.one = map.two = map.three = "";
+  quiz.classList.add("hide");
+});
+
+function result() {
+  if (map.two === map.three) {
+    quiz_title.textContent = `You got: ${RESULTS_MAP[map.two].title}`;
+    quiz_desc.textContent = RESULTS_MAP[map.two].contents;
+  } else {
+    quiz_title.textContent = `You got: ${RESULTS_MAP[map.one].title}`;
+    quiz_desc.textContent = RESULTS_MAP[map.one].contents;
+  }
+  quiz.classList.remove("hide");
 }
