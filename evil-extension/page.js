31899789dc6043297a608ba22831984ee3120cd6
mengru <4momolmr@gmail.com>
2019-04-28 13:31:58 +0800

    part 2 finished

--- evil-extension/page.js
+++ evil-extension/page.js
@@ -12,6 +12,25 @@ const MATCH_LIST = {
 
 function transformTextNodes(node) {
   // TODO(you): Implement this function! See HW spec for details.
+    console.log("name="+node.nodeValue);
+  if(node.nodeName==="SCRIPT"||node.nodeName==="STYLE")return;
+	if(node.nodeValue!=null){
+    console.log(node.nodeValue);
+		let textArray=node.nodeValue.split(' ');
+		for(let i=0,length=textArray.length;i<length;i++){
+			textArray[i]=textArray[i].replace('\n','');
+			textArray[i]=textArray[i].trim();
+			if(textArray[i] in MATCH_LIST){
+        console.log(textArray[i]);
+        console.log(MATCH_LIST[textArray[i]]);
+        textArray[i]=MATCH_LIST[textArray[i]];
+      }
+		}
+		node.nodeValue=textArray.join(' ');
+	}
+	let children=node.childNodes
+	for(let i=0,length=children.length;i<length;i++)
+		transformTextNodes(children[i]);
 }
 
 transformTextNodes(document.body);
